cff20a436e68ea3f640e35948eec1b4987973844
atlas25git <atlas.sarthak@gmail.com>
2021-07-16 14:28:40 +0530

    PORT

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { MONGODB } = require('./config.js');
 
 const pubsub = new PubSub();
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
